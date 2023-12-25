2b44e64cf2d25eda60e5c9c53f1da3d1118abdbb
TonWonton <154277764+TonWonton@users.noreply.github.com>
2023-12-25 22:09:20 +0100

    Delete HC_HGaugeCtrlNew directory

--- HC_HGaugeCtrlNew/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using HC_HGaugeAndSpeedCtrl;
-
-[assembly: AssemblyTitle(HC_HGaugeAndSpeedCtrl.HGaugeAndSpeedCtrl.PluginName)]
-[assembly: AssemblyDescription(HC_HGaugeAndSpeedCtrl.HGaugeAndSpeedCtrl.PluginName)]
-[assembly: AssemblyCompany("https://github.com/TonWonton/HC_HSceneCtrl")]
-[assembly: AssemblyProduct(HC_HGaugeAndSpeedCtrl.HGaugeAndSpeedCtrl.GUID)]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyVersion(HC_HGaugeAndSpeedCtrl.HGaugeAndSpeedCtrl.PluginVersion)]
